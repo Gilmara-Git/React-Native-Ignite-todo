273a3b2d59a422f8f2424b1803b893a61d7d0b2e
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-02-03 19:18:42 -0500

    feat: setup the themes files for colors and fonts

---
+++ src/themes/themes.ts
@@ -0,0 +1,20 @@
+export default {
+    colors:{
+        gray200: '#D9D9D9',
+        gray300: '#808080',
+        gray400: '#333333',
+        gray700: '#0D0D0D',
+        gray700Desc_1: '#171717',
+        gray700Desc_2: '#272727',
+        gray700Desc_3: '#333333',
+        background: '#454545',
+        blueDarkButton: '#1E6F9F',
+        purple: '#8284FA',
+        blue: '#4EA8DE'
+    },
+    fonts:{
+        InterRegular400 : 'Inter_400Regular',
+        InterBold700: 'Inter_700Bold'
+
+    }
+}
