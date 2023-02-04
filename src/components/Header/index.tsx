594b8c77e98ff77acd392be7f7bf5c65814094c9
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-02-03 20:57:22 -0500

    feat: finished Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,23 @@
-import { View , Text} from 'react-native';
-import styles from './styles';
+import { View, Image } from "react-native";
+import styles from "./styles";
+import Rocket from "../../assets/rocket.svg";
+import To from "../../assets/to.svg";
+import Do from "../../assets/do.svg";
 
-export const Header =()=>{
-    return (
+export const Header = () => {
+  return (
     <View style={styles.container}>
-     <Text style={{color: 'white'}}>SE</Text> 
-
-    </View>)
+      <View style={styles.rocket}>
+        <Rocket />
+      </View>
+      <View style={styles.toDo}>
+        <View style={styles.to}>
+          <To />
+        </View>
+        <View style={styles.do}>
+          <Do />
+        </View>
+      </View>
+    </View>
+  );
 };
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,9 +6,27 @@ const styles = StyleSheet.create({
     container:{     
         backgroundColor: themes.colors.gray700,
         height: 175,
+        flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center'
     },
+    rocket:{
+        width: 19,
+        height: 31,
+        marginRight:10
+    },
+    toDo:{
+        flexDirection: 'row',
+    },
+    to:{
+        width: 34,
+        height: 24,
+    },
+    do:{
+        width: 42,
+        height: 26,
+        marginLeft: 7  
+    }
    
 })
 
