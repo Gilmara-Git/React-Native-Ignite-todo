318cac171736fd71bebf4d770962a3118201ef33
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2023-02-07 22:14:42 -0500

    Added on addTodo function into the DataEntry component

--- src/components/DataEntry/index.tsx
+++ src/components/DataEntry/index.tsx
@@ -4,16 +4,29 @@ import styles from './styles';
 import themes from '../../themes/themes';
 import Plus from "../../assets/plus.svg";
 
-export const DataEntry =()=>{
-    const [ inputFocusedColor , setInputFocusedColor ] = useState('');
+interface DataEntryProps {
+  onAddTodo: (todo: string)=>void;
+}
+
+export const DataEntry =({onAddTodo}:DataEntryProps)=>{
+    const [ inputFocusedColor , setInputFocusedColor ] = useState('transparent');
+    const [enteredTodo, setEnteredTodo ] = useState('');
     
     const focusedInputBorderColor =()=>{
         setInputFocusedColor(themes.colors.purple);
     }
     const blurredInputBorderColor =()=>{
-        setInputFocusedColor('');
+        setInputFocusedColor('transparent');
     }
 
+    const todoHandler =(text: string)=>{
+      setEnteredTodo(text);
+    }
+
+    const sendTodo= ()=>{
+      onAddTodo(enteredTodo);
+      setEnteredTodo('');
+    }
     return (
         <View style={styles.inputButtonHolder}>
         <TextInput
@@ -22,8 +35,15 @@ export const DataEntry =()=>{
           placeholderTextColor={themes.colors.gray300}
           onFocus={focusedInputBorderColor}
           onBlur={blurredInputBorderColor}
+          onChangeText={todoHandler}
+          value={enteredTodo}
         />
-        <TouchableOpacity activeOpacity={0.7} style={styles.button}>
+        <TouchableOpacity 
+          activeOpacity={0.7} 
+          style={styles.button}
+          onPress={sendTodo}
+          >
+
           <Plus />
         </TouchableOpacity>
       </View>
